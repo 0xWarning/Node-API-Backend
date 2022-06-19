616615f5eeb17ebebdcb9a8c95f246427e0c7198
0xWarning <65122657+0xWarning@users.noreply.github.com>
2022-06-19 13:30:23 +0100

    Minor Changes
    Upload file

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 //Required
 const express = require("express");
+const fileUpload = require('express-fileupload');
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const bcrypt = require('bcrypt');
 const User = require("./modal/user");
 
 const app = express();
+
+
+app.use(fileUpload({
+    createParentPath: true
+}));
 
 require("dotenv/config");
 
@@ -132,6 +138,38 @@ app.post("/admin/rem", async (req, res) => {
         res.status(200).send("User Deleted");
       })
 })
+
+app.post('/upload-avatar', async (req, res) => {
+    try {
+        if(!req.files) {
+            res.send({
+                status: false,
+                message: 'No file uploaded'
+            });
+        } else {
+            //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
+            let avatar = req.files.avatar;
+            
+            //Use the mv() method to place the file in upload directory (i.e. "uploads")
+            avatar.mv('./uploads/' + avatar.name);
+
+            //send response
+            res.send({
+                status: true,
+                message: 'File is uploaded',
+                data: {
+                    name: avatar.name,
+                    mimetype: avatar.mimetype,
+                    size: avatar.size
+                }
+            });
+
+            console.log(`[UPL] [POST] A File was uploaded`);
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
 
 // Connect DB
 
